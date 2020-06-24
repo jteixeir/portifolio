cf0b191475965bb90bdc18fd7fb043d25725bbd4
jteixeir <jessica.tsousa@gmail.com>
2020-06-24 16:47:21 -0300

    my portifolio development day 05

--- js/main.js
+++ js/main.js
@@ -1,13 +1,48 @@
-$(document).ready(function(){
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+$(document).ready(function () {
 
     $nav = $('.nav');
-    $toggleCollapse=$('.toggle-collapse');
+    $toggleCollapse = $('.toggle-collapse');
 
-    /*click event on toggle menu*/
-    $toggleCollapse.click(function(){
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
 
-    //owl carousel for blog
-    $('.owl-carousel').owlCarousel();
+    // owl-crousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: responsive
+    });
+
+
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+    // AOS Instance
+    AOS.init();
+
 });
